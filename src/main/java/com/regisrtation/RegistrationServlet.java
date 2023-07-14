8dfbefb7cfcabbeddab0d7f8b20d9b3b86da4f22
Manish Manker <manishmanker877@gmail.com>
2023-07-14 22:32:36 +0530

    All Files are uploded

---
+++ src/main/java/com/regisrtation/RegistrationServlet.java
@@ -0,0 +1,97 @@
+package com.regisrtation;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String reupwd = request.getParameter("re_pass");
+		String umobile = request.getParameter("contact");
+        
+		Connection con = null;
+		RequestDispatcher dispatcher = null;
+		
+		if(uname == null || uname=="") {
+			request.setAttribute("status", "invalid name");
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			dispatcher.forward(request, response);
+		}
+		if(uemail == null || uemail=="") {
+			request.setAttribute("status", "invalid Email");
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			dispatcher.forward(request, response);
+		}
+		if(upwd == null || upwd=="") {
+			request.setAttribute("status", "invalid passward");
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			dispatcher.forward(request, response);
+		}
+		if(!(upwd.equals(reupwd))) {
+			request.setAttribute("status", "invalid repassward");
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			dispatcher.forward(request, response);
+		}
+		if(umobile == null || umobile=="" || umobile.length()<9) {
+			request.setAttribute("status", "invalid Mobile_no");
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		
+		
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/logdata","root","manish@db");
+			PreparedStatement pst = con.prepareStatement("insert into users (uname,upwd,uemail,umobile) values (?,?,?,?)");
+			
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowcount = pst.executeUpdate();
+			
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if(rowcount>0) {
+				request.setAttribute("status", "success");
+			}else {
+				request.setAttribute("status", "failed");
+			}
+			dispatcher.forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+	}
+
+}
